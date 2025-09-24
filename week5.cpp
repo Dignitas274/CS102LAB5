c658a208ea85dde2fd69ab863762d86aa05e7924
Dignitas <250075@newuu.uz>
2025-09-24 12:49:59 +0500

    5 solved :(

---
+++ week5.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+
+
+//problem1
+/*
+    for (int i = 1; i<=10; i++) {
+        cout << i << " ";
+}
+*/
+//problem2
+/*
+    int sum = 0;
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+        sum += i;
+    }
+    cout << "\nSum = " << sum << endl;
+*/
+//problem3
+/*
+    int n;
+    cout << "n= ";
+    cin >> n;
+    for (int i = 10; i <= n; i += 10) {
+        cout << i << " ";
+    }
+*/
+//problem4
+/*
+    int n;
+    cout << "n= ";
+    cin >> n;
+
+    for (int i = 10; i <= n; i += 10) {
+        cout << i;
+        if (i + 10 <= n) {
+            cout << ", ";
+        }
+    }
+*/
+//problem5
+/*
+    int count = 0;
+    for (char ch = 'A'; ch <= 'Z'; ch++) {
+        cout << ch << " ";
+        count++;
+        if (count % 5 == 0) {
+            cout << endl;
+        }
+    }
+*/
+
+
+
+
+
+
+
+
+    return 0;
+}
